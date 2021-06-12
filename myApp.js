c64c5b553212219ef72777c89835eb30bbc3a82f
Marcel van Remmerden <mvanremmerden@gitlab.com>
2021-06-12 14:07:15 +0200

    Add singular option for time since

--- myApp.js
+++ myApp.js
@@ -199,24 +199,34 @@ function escapeHtml(unsafe) {
 function timeSince(date) {
     var seconds = Math.floor((new Date() - date) / 1000);
     var interval = seconds / 31536000;
-    if (interval > 1) {
+    if (interval >= 2) {
         return Math.floor(interval) + " years";
+    } else if (interval > 1 && interval < 2) {
+        return Math.floor(interval) + " year";
     }
     interval = seconds / 2592000;
-    if (interval > 1) {
+    if (interval > 2) {
         return Math.floor(interval) + " months";
+    } else if (interval > 1 && interval < 2) {
+        return Math.floor(interval) + " month";
     }
     interval = seconds / 86400;
-    if (interval > 1) {
+    if (interval > 2) {
         return Math.floor(interval) + " days";
+    } else if (interval > 1 && interval < 2) {
+        return Math.floor(interval) + " day";
     }
     interval = seconds / 3600;
-    if (interval > 1) {
+    if (interval >= 2) {
         return Math.floor(interval) + " hours";
+    } else if (interval > 1 && interval < 2) {
+        return Math.floor(interval) + " hour";
     }
     interval = seconds / 60;
-    if (interval > 1) {
+    if (interval > 2) {
         return Math.floor(interval) + " minutes";
+    } else if (interval > 1 && interval < 2) {
+        return Math.floor(interval) + " minute";
     }
     return Math.floor(seconds) + " seconds";
 }
