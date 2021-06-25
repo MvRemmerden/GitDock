49727ee7191065baf5330e7d2f887994874fcdea
Marcel van Remmerden <maclbb@hotmail.de>
2021-06-25 21:58:39 +0200

    Add project and group links to bookmarks

--- myApp.js
+++ myApp.js
@@ -8,7 +8,8 @@ const { URL } = require('url')
 
 const typeParsers = {
     'merge_requests': merge_requests => ({ merge_requests: parseInt(merge_requests, 10) }),
-    'issues': issues => ({ issue: parseInt(issues, 10) }),
+    'issues': issues => ({ issues: parseInt(issues, 10) }),
+    'epics': epics => ({ epics: parseInt(epics, 10) }),
     'tree': branch => ({ branch }),
     'commits': branch => ({ branch }),
     'commit': commits => ({ commit })
@@ -38,7 +39,7 @@ const mb = menubar({
     icon: __dirname + '/assets/gitlab.png',
     preloadWindow: true,
     browserWindow: {
-        width: 600,
+        width: 1000,
         height: 650,
         webPreferences: {
             preload: __dirname + '/preload.js',
@@ -56,7 +57,6 @@ if (access_token && user_id && username) {
         //Preloading content
         getUser()
         getLastCommits()
-        getRecentlyVisited()
         getRecentComments()
         getUsersProjects()
         getBookmarks()
@@ -70,7 +70,7 @@ if (access_token && user_id && username) {
             getBookmarks()*/
         }, 120000);
 
-        //mb.window.webContents.openDevTools()
+        mb.window.webContents.openDevTools()
         ipcMain.on('detail-page', (event, arg) => {
             mb.window.webContents.executeJavaScript('document.getElementById("detail-headline").innerHTML = ""')
             mb.window.webContents.executeJavaScript('document.getElementById("detail-content").innerHTML = ""')
@@ -580,8 +580,14 @@ function getBookmarks() {
     if (bookmarks && bookmarks.length > 0) {
         bookmarksString = '<ul class=\\"list-container\\">'
         bookmarks.forEach(bookmark => {
+            let namespace = ''
+            if(bookmark.namespace) {
+                namespace = '<a href=\\"' + bookmark.locationUrl + '\\" target=\\"_blank\\">' + bookmark.namespace + ' / ' + bookmark.project + '</a>'
+            }else{
+                namespace = '<a href=\\"' + bookmark.locationUrl + '\\" target=\\"_blank\\">' + bookmark.project + '</a>'
+            }
             let bookmarkUrl = "'" + bookmark.url + "'"
-            bookmarksString += '<li class=\\"history-entry bookmark-entry\\"><div class=\\"bookmark-information\\"><a href=\\"' + bookmark.url + '\\" target=\\"_blank\\">' + bookmark.title + '</a><span class=\\"namespace-with-time\\">Added ' + timeSince(bookmark.added) + ' ago &middot; ' + bookmark.project + ' / ' + bookmark.namespace + '</span></div><div class=\\"bookmark-delete-wrapper\\"><div class=\\"bookmark-delete\\" onclick=\\"deleteBookmark(' + bookmarkUrl + ')\\"><svg xmlns=\\"http://www.w3.org/2000/svg\\" viewBox=\\"0 0 16 16\\"><path fill=\\"#c9d1d9\\" d=\\"M14,3 C14.5522847,3 15,3.44771525 15,4 C15,4.55228475 14.5522847,5 14,5 L13.846,5 L13.1420511,14.1534404 C13.0618518,15.1954311 12.1930072,16 11.1479,16 L4.85206,16 C3.80698826,16 2.93809469,15.1953857 2.8579545,14.1533833 L2.154,5 L2,5 C1.44771525,5 1,4.55228475 1,4 C1,3.44771525 1.44771525,3 2,3 L5,3 L5,2 C5,0.945642739 5.81588212,0.0818352903 6.85073825,0.00548576453 L7,0 L9,0 C10.0543573,0 10.9181647,0.815882118 10.9945142,1.85073825 L11,2 L11,3 L14,3 Z M11.84,5 L4.159,5 L4.85206449,14.0000111 L11.1479,14.0000111 L11.84,5 Z M9,2 L7,2 L7,3 L9,3 L9,2 Z\\"/></svg></div></div></li>'
+            bookmarksString += '<li class=\\"history-entry bookmark-entry\\"><div class=\\"bookmark-information\\"><a href=\\"' + bookmark.url + '\\" target=\\"_blank\\">' + bookmark.title + '</a><span class=\\"namespace-with-time\\">Added ' + timeSince(bookmark.added) + ' ago &middot; ' + namespace + '</span></div><div class=\\"bookmark-delete-wrapper\\"><div class=\\"bookmark-delete\\" onclick=\\"deleteBookmark(' + bookmarkUrl + ')\\"><svg xmlns=\\"http://www.w3.org/2000/svg\\" viewBox=\\"0 0 16 16\\"><path fill=\\"#c9d1d9\\" d=\\"M14,3 C14.5522847,3 15,3.44771525 15,4 C15,4.55228475 14.5522847,5 14,5 L13.846,5 L13.1420511,14.1534404 C13.0618518,15.1954311 12.1930072,16 11.1479,16 L4.85206,16 C3.80698826,16 2.93809469,15.1953857 2.8579545,14.1533833 L2.154,5 L2,5 C1.44771525,5 1,4.55228475 1,4 C1,3.44771525 1.44771525,3 2,3 L5,3 L5,2 C5,0.945642739 5.81588212,0.0818352903 6.85073825,0.00548576453 L7,0 L9,0 C10.0543573,0 10.9181647,0.815882118 10.9945142,1.85073825 L11,2 L11,3 L14,3 Z M11.84,5 L4.159,5 L4.85206449,14.0000111 L11.1479,14.0000111 L11.84,5 Z M9,2 L7,2 L7,3 L9,3 L9,2 Z\\"/></svg></div></div></li>'
         })
         bookmarksString += '<li id=\\"add-bookmark-dialog\\" class=\\"more-link\\"><a onclick=\\"startBookmarkDialog()\\">Add another bookmark <svg xmlns=\\"http://www.w3.org/2000/svg\\" viewBox=\\"0 0 16 16\\"><path fill=\\"#aaa\\" fill-rule=\\"evenodd\\" d=\\"M10.7071,7.29289 C11.0976,7.68342 11.0976,8.31658 10.7071,8.70711 L7.70711,11.7071 C7.31658,12.0976 6.68342,12.0976 6.29289,11.7071 C5.90237,11.3166 5.90237,10.6834 6.29289,10.2929 L8.58579,8 L6.29289,5.70711 C5.90237,5.31658 5.90237,4.68342 6.29289,4.29289 C6.68342,3.90237 7.31658,3.90237 7.70711,4.29289 L10.7071,7.29289 Z\\"/></svg></a></li></ul>'
         mb.window.webContents.executeJavaScript('document.getElementById("bookmarks").innerHTML = "' + bookmarksString + '"')
@@ -704,16 +710,37 @@ function startBookmarkDialog() {
 
 async function parseGitLabUrl(link) {
     let object = parse(link)
-    let result = await fetch('https://gitlab.com/api/v4/projects/' + encodeURIComponent(object.namespace + '/' + object.project) + '/issues/' + object.issue + '?access_token=' + access_token)
-    let issuable = await result.json()
-    let result2 = await fetch('https://gitlab.com/api/v4/projects/' + issuable.project_id + '?access_token=' + access_token)
-    let project = await result2.json()
-    return {
-        url: link,
-        namespace: project.namespace.name,
-        project: project.name,
-        title: issuable.title,
-        added: Date.now()
+    console.log(object)
+    let issuable 
+    if(object.type == 'issues' || object.type == 'merge_requests') {
+        let result = await fetch('https://gitlab.com/api/v4/projects/' + encodeURIComponent(object.namespace + '/' + object.project) + '/' + object.type + '/' + object[object.type] + '?access_token=' + access_token)
+        issuable = await result.json()
+        let result2 = await fetch('https://gitlab.com/api/v4/projects/' + issuable.project_id + '?access_token=' + access_token)
+        let project = await result2.json()
+        console.log(project)
+        return {
+            url: link,
+            namespace: project.namespace.name,
+            project: project.name,
+            title: issuable.title,
+            added: Date.now(),
+            type: object.type,
+            locationUrl: project.web_url
+        }
+    }else if(object.type == 'epics') {
+        let result = await fetch('https://gitlab.com/api/v4/groups/' + encodeURIComponent(object.project) + '/' + object.type + '/' + object[object.type])
+        issuable = await result.json()
+        let result2 = await fetch('https://gitlab.com/api/v4/groups/' + issuable.group_id + '?access_token=' + access_token)
+        let group = await result2.json()
+        console.log(group)
+        return {
+            url: link,
+            project: group.name,
+            title: issuable.title,
+            added: Date.now(),
+            type: object.type,
+            locationUrl: group.web_url
+        }
     }
 }
 
@@ -724,7 +751,7 @@ function parse(gitlabUrl) {
     const url = new URL(gitlabUrl)
     const path = url.pathname
     const [, namespace, project, deliminator, type, ...rest] = path.split('/')
-    let result = { namespace, project }
+    let result = { namespace, project, type }
     const typeParser = typeParsers[type]
     if (typeParser) {
         result = { ...result, ...typeParser(rest.shift()) }
