954677852dd0109e3f99e26028c2001f5795aded
Marcel van Remmerden <mvanremmerden@gitlab.com>
2022-07-01 17:35:29 +0200

    Increase timeout duration

--- test/launch.spec.js
+++ test/launch.spec.js
@@ -3,7 +3,7 @@ const test = require('@playwright/test').test;
 const { newApp, stopAppAfterEach } = require('./util');
 
 test.describe('Application launch', function () {
-  test.setTimeout(30000);
+  test.setTimeout(120000);
 
   test.beforeEach(async function () {
     await newApp(this);
